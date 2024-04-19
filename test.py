ef762bac2b1b319d16c4ea4e168b28da5fca6e0a
1066480 <128582967+1066480@users.noreply.github.com>
2024-04-19 09:37:17 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -289,7 +289,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='D:/lixueyan/yolov7-main/yolov7-main/runs/train/exp18/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
     # parser.add_argument('--weights', nargs='+', t7.9ype=str,default='D:/lixueyan/yolov7-main/yolov7-main/runs/train/exp20/weights/best.pt',help='model.pt path(s)')
     # parser.add_argument('--data', type=str, default='data/mydata.yaml', help='*.data path')
     parser.add_argument('--data', type=str, default='data/CEW.yaml', help='*.data path')
